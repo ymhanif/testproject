2c9f4f440863f5ea5b44e8c3e3e3e640fb498ee6
ymhanif <ymhanif07@gmail.com>
2016-08-30 10:05:39 +0000

    Button and Snackbar

--- app/src/main/java/com/labs/testproject/MainActivity.java
+++ app/src/main/java/com/labs/testproject/MainActivity.java
@@ -1,13 +1,35 @@
 package com.labs.testproject;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button clickMe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void intiLayout(){
+        clickMe = (Button) findViewById(R.id.Btn_ClickMe);
+
+        clickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Congratulations. You clicked a button", Snackbar.LENGTH_LONG)
+                        .setAction("Dismiss", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // The snackbar is automatically dismissed, so you add
+                                // whatever additional tasks to be performed
+                            }
+                        }).show();
+            }
+        });
     }
 }
